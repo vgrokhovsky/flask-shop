01dec842434d05efdc16f5e811528fb0a1064cc4
Morgarinka <freeswolf@gmail.com>
2025-05-13 22:48:20 +0300

    ttt

---
+++ app/forms.py
@@ -0,0 +1,4 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, FloatField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo
+
---
+++ app/templates/auth/auth.py
@@ -0,0 +1,43 @@
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
+from db import models, db, User
+from . import auth
+from forms import RegistrationForm, LoginForm
+
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created for {}".format(form.username.data), category="success")
+        return redirect(url_for("auth.login"))
+    return render_template("register.html", form=form)
+
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.password == form.password.data:
+            login_user(user)
+            return redirect(url_for("main.index"))
+        else:
+            flash(
+                "Login Unsuccessful. Please check email and password", category="danger"
+            )
+    return render_template("login.html", form=form)
+
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("auth.login"))
