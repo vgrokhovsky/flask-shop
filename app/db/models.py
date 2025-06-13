2c81ac828b55c1b942d1526e51bd668ec7f7aa96
Morgarinka <freeswolf@gmail.com>
2025-06-13 20:24:20 +0300

    Лицный кабинет и корзтна работает

--- app/db/models.py
+++ app/db/models.py
@@ -1,6 +1,7 @@
 from flask_login import UserMixin
 
-from . import db_object as db 
+from . import db_object as db
+
 
 class User(db.Model, UserMixin):
     __tablename__ = "users"
@@ -11,7 +12,8 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String, unique=True, nullable=False)
     cart_items = db.relationship("Cartitem", back_populates="user", lazy=True)
 
-    products = db.relationship('Product', backref='user', lazy=True)
+    products = db.relationship("Product", backref="user", lazy=True)
+
 
 class Product(db.Model):
     __tablename__ = "product"
@@ -23,17 +25,19 @@ class Product(db.Model):
     stock = db.Column(db.String, nullable=False)
     image_path = db.Column(db.String, nullable=False)
 
-    user = db.relationship('User', backref='products')
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
     cart_items = db.relationship("Cartitem", back_populates="product")
+   
 
 
 class Cartitem(db.Model):
     __tablename__ = "cartitem"
-    
+
     id = db.Column(db.Integer, primary_key=True)
     quantity = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey("product.id"), nullable=False)
 
-    user = db.relationship('User', back_populates='cart_items')
-    product = db.relationship('Product', back_populates='cart_items')
+    user = db.relationship("User", back_populates="cart_items")
+    product = db.relationship("Product", back_populates="cart_items")
